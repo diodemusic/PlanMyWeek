5df4369b8615278b9771c348317a5aee557730f2
diodemusic <diodemusicuk@gmail.com>
2025-06-01 00:52:50 +0100

    Refactor lesson numbering in get_lessons method and update get_week return type

--- src/plan.py
+++ src/plan.py
@@ -1,6 +1,7 @@
 # TODO: add week header to MD
 # week: int = 8 # int(input("What week is it? "))
 # self.markdown += f"# Weekly Plan - Week {week}\n\n"
+# TODO: let user choose lesson 0.x number
 
 class Plan:
     def __init__(self, modules: list[str]) -> None:
@@ -15,14 +16,15 @@ class Plan:
         for module in self.modules:
             lessons_amount: int = 3 # int(input(f"How many lessons do you have this week for {module}: "))
             lessons_num: int = 4 # int(input(f"What's the lesson number for {module}? "))
+            lessons_decimal_num: int = 4 # int(input(f"What's the first lessons decimal number for {module}? "))
 
             for i in range(lessons_amount):
-                lessons.append({module: f"Lesson {lessons_num}.{i + 1}"})
+                lessons.append({module: f"Lesson {lessons_num}.{lessons_decimal_num + i}"})
 
         return lessons
 
-    def get_week(self, lessons: list[dict[str, str]]) -> dict[str, list[str]]:
-        week: dict[str, list[str]] = {
+    def get_week(self, lessons: list[dict[str, str]]) -> dict[str, list[dict[str, str]]]:
+        week: dict[str, list[dict[str, str]]] = {
             "Monday": [],
             "Tuesday": [],
             "Wednesday": [],
@@ -41,3 +43,6 @@ class Plan:
                 week[days[i]].append(lessons[i + 1])
 
         return week
+
+    def get_markdown(self, week: dict[str, list[dict[str, str]]]) -> str:
+        pass
